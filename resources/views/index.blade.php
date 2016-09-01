4cc1799ae39cae9148dc8cd2e7c8ab43dd28a6ab
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2016-09-01 00:55:33 -0500

    Integrate html y view laravel home

---
+++ resources/views/index.blade.php
@@ -0,0 +1,205 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: oscarsith
+ * Date: 1/09/16
+ * Time: 12:38 AM
+ */?>
+@extends('layouts.master')
+
+@section('content')
+<!-- #testimonials -->
+<div id="testimonials" class="bg-grey wrap-container80">
+
+    <div class="container">
+        <i class="fa fa-quote-left affa-testimonial-icon"></i>
+    </div>
+
+    <!-- .carousel-slider -->
+    <div class="carousel-slider affa-testimonials-carousel">
+
+        <div class="slick-slide"> <!-- 1 -->
+            <div class="container">
+                <div class="affa-testimonial">
+                    <div class="testimonial-txt">
+                        <p>"Lorem ipsum dolor consectetur adipiscing elit, sed do eiusmod tempor incididunt labore et dolore magna aliqua. Enim id minim veniam exercitation laboris consequat. Duis aute dolor reprehende ritin voluptate cillum fugiatos."</p>
+                    </div>
+                    <div class="testimonial-name">
+                        <img src="images/content/avatar/1.jpg" alt="Avatar">
+                        <h4>Amah Holland</h4>
+                        <p>Social Media Manager of MG</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slick-slide"> <!-- 2 -->
+            <div class="container">
+                <div class="affa-testimonial">
+                    <div class="testimonial-txt">
+                        <p>"Lorem ipsum dolor consectetur adipiscing elit, sed do eiusmod tempor incididunt labore et dolore magna aliqua. Enim id minim veniam exercitation laboris consequat. Duis aute dolor reprehende ritin voluptate cillum fugiatos."</p>
+                    </div>
+                    <div class="testimonial-name">
+                        <img src="images/content/avatar/2.jpg" alt="Avatar">
+                        <h4>Natsu Dragnell</h4>
+                        <p>Lead Designer of DolanDolen</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="slick-slide"> <!-- 3 -->
+            <div class="container">
+                <div class="affa-testimonial">
+                    <div class="testimonial-txt">
+                        <p>"Lorem ipsum dolor consectetur adipiscing elit, sed do eiusmod tempor incididunt labore et dolore magna aliqua. Enim id minim veniam exercitation laboris consequat. Duis aute dolor reprehende ritin voluptate cillum fugiatos."</p>
+                    </div>
+                    <div class="testimonial-name">
+                        <img src="images/content/avatar/3.jpg" alt="Avatar">
+                        <h4>Denden Mushi</h4>
+                        <p>Mobile Developer of Avelie</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- .carousel-slider end -->
+
+</div>
+<!-- #testimonials end -->
+
+<!-- #features -->
+<div id="features" class="padding-top80">
+
+    <!-- .container -->
+    <div class="container">
+
+        <div class="post-heading-center">
+            <h2>Get More Paying Customers</h2>
+            <p>Take control over your landing page template.</p>
+        </div>
+
+
+        <!-- .row -->
+        <div class="row padding-bottom20">
+
+            <div class="col-sm-6 col-md-5 margin-bottom40">
+                <figure><img src="images/content/landing/apple-devices.png" alt="Image"></figure>
+            </div>
+
+            <div class="col-sm-6 col-md-7 col-lg-6 col-lg-offset-1 margin-bottom20">
+                <div class="col-txt100">
+
+                    <div class="post-heading-left post-heading-top-desc margin-bottom20">
+                        <p>Conoce acerca de Nosotros</p>
+                        <h2>Cómo impactamos en tus <strong>aspiraciones</strong>?</h2>
+                    </div>
+
+                    <p>En REUNEOS puedes encontrar personas que tienen intereses profesioanles similares a los tuyos y con los que puedes llevar a cabo un encuentro formal y de la manera más fácil, logrando:</p>
+
+                    <div class="list-icon">
+                        <ul>
+                            <li><i class="fa fa-check-square-o"></i> Crear socios estratégicos para tu negocio o emprendimiento.</li>
+                            <li><i class="fa fa-check-square-o"></i> Concretar actividades comerciales de venta o compra.</li>
+                            <li><i class="fa fa-check-square-o"></i> Encontrar oportunidades que jamás pensaste tener.</li>
+                            <li><i class="fa fa-check-square-o"></i> Todo lo anterior de la manera más práctica y segura.</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- .row end -->
+
+        <!-- .row -->
+        <div class="row">
+
+            <div class="col-md-6 margin-bottom20">
+                <div class="col-txt120">
+
+                    <div class="post-heading-left text-center-sm">
+                        <h2><strong>Great support.</strong> We'll help you!</h2>
+                        <p>Our customer service won’t leave you alone</p>
+                    </div>
+
+                    <p class="margin-bottom20 text-center-sm">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempam justo duo dolores aliqua dolore rebum.</p>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="affa-col-txt">
+                                <h4>Support step 1</h4>
+                                <p>Duis aute dolor reprehende ritin voluptate cillum dolore fugiatos pariatur sint occaecat est.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="affa-col-txt">
+                                <h4>Support step 2</h4>
+                                <p>Duis aute dolor reprehende ritin voluptate cillum dolore fugiatos pariatur sint occaecat est.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="affa-col-txt">
+                                <h4>Support step 3</h4>
+                                <p>Duis aute dolor reprehende ritin voluptate cillum dolore fugiatos pariatur sint occaecat est.</p>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="affa-col-txt">
+                                <h4>Support step 4</h4>
+                                <p>Duis aute dolor reprehende ritin voluptate cillum dolore fugiatos pariatur sint occaecat est.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-sm-8 col-md-6 col-sm-offset-2 col-md-offset-0">
+                <figure><img src="images/content/landing/great-support.png" alt="Image"></figure>
+            </div>
+
+        </div>
+        <!-- .row end -->
+
+    </div>
+    <!-- .container end -->
+
+</div>
+<!-- #features end -->
+
+
+<!-- #bottom -->
+<div id="bottom" class="bg-img">
+
+    <!-- .bg-overlay -->
+    <div class="bg-overlay">
+
+        <!-- .container -->
+        <div class="container">
+
+            <div class="post-heading-center">
+                <h2>Subscribe for The Latest Offers!</h2>
+                <p>Join thousands of satisfied subscribers to get the latest offers from us.</p>
+            </div>
+
+            <form method="post" action="#" class="affa-form-subscribe">
+                <input type="text" name="email" placeholder="Email address....">
+                <input type="submit" name="submit" value="Subscribe Now">
+            </form>
+
+            <p class="txt-desc">* We don’t share your personal info with anyone. Check out our <a href="#">Privacy Policy</a> for more information.</p>
+
+        </div>
+        <!-- .container end -->
+
+    </div>
+    <!-- .bg-overlay -->
+
+    <div class="bg-img-base" style="background-image:url(images/content/bg/3.jpg);"></div> <!-- background image -->
+</div>
+<!-- #bottom end -->
+@endsection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="ie ie6" lang="en"><![endif]-->
+<!--[if IE 7]><html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8]><html class="ie ie8" lang="en"><![endif]-->
+<!--[if IE 9]><html class="ie9" lang="en"><![endif]-->
+<!--[if (gte IE 10)|!(IE)]><!--><html lang="en"><!--<![endif]-->
+<head>
+
+    <!-- Your Basic Site Informations -->
+    <title>Reuneos</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Conversi is a professional conversion landing page that built-in fully responsive template, premium design, elegant style and have a slew of features.">
+    <meta name="keywords" content="Landing Page, One Page Template, Premium Design, Responsive Template, Conversion Landing Page, Conversion Template, Registration Form, Elegant Style, PSD, HTML5, CSS3, jQuery">
+    <meta name="author" content="Affapress">
+
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,300italic,400italic,700" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:400,300,500,600,700" rel="stylesheet" type="text/css">
+
+    <!-- Stylesheets -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <link rel="stylesheet" href="css/jquery.fancybox.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- Custom Colors -->
+    <!--<link rel="stylesheet" href="css/colors/blue/color.css">-->
+    <!--<link rel="stylesheet" href="css/colors/green/color.css">-->
+    <!--<link rel="stylesheet" href="css/colors/pink/color.css">-->
+    <!--<link rel="stylesheet" href="css/colors/purple/color.css">-->
+    <!--<link rel="stylesheet" href="css/colors/yellow/color.css">-->
+
+    <!--[if lt IE 9]>
+    <script src="js/html5.js"></script>
+    <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+    <!--[if lt IE 8]>
+    <link rel="stylesheet" href="css/ie-older.css">
+    <![endif]-->
+
+    <noscript><link rel="stylesheet" href="css/no-js.css"></noscript>
+
+    <!-- Favicons -->
+    <link rel="shortcut icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="images/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple-touch-icon-114x114.png">
+
+</head>
+<body>
+
+<!-- #header -->
+<header id="header" data-parallax="scroll" data-speed="0.2" data-natural-width="1920" data-natural-height="1080" data-image-src="images/content/bg/1.jpg">
+
+    <!-- .header-overlay -->
+    <div class="header-overlay">
+
+        <!-- #navigation -->
+        <nav id="navigation" class="navbar scrollspy">
+
+            <!-- .container -->
+            <div class="container">
+
+                <div class="navbar-brand">
+                    <a href="/"><img src="images/logo.png" alt="Logo"></a> <!-- site logo -->
+                </div>
+
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#header" class="smooth-scroll">INICIO</a></li>
+                    <li><a href="#testimonials" class="smooth-scroll">TESTIMONIOS</a></li>
+                    <li><a href="#features" class="smooth-scroll">NOSOTROS</a></li>
+                    <li><a href="home">INICIE SESIÓN</a></li>
+                    <li class="menu-btn"><a href="login">ÚNASE AHORA!</a></li>
+                </ul>
+
+            </div>
+            <!-- .container end -->
+
+        </nav>
+        <!-- #navigation end -->
+
+        <!-- .header-content -->
+        <div class="header-content">
+
+            <!-- .container -->
+            <div class="container">
+
+                <!-- .row -->
+                <div class="row">
+
+                    <div class="col-sm-6 col-md-7 col-lg-6">
+                        <div class="header-txt">
+                            <h1>En REUNEOS encontrarás la mejor forma de realizar contactos de verdad con citas de verdad.</h1>
+                            <h4>Tenemos la mejor forma de hacer contactos efectivos para tu emprendimiento, proyecto o negocio compartiendo con quien quieres tus intereses.</h4>
+                            <div class="header-txt-btn">
+                                <a href="https://www.youtube.com/embed/4ZawV1mXlS8?autoplay=1" class="btn-play fancybox-media" title="Play Video"><i class="fa fa-play"></i></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-5 col-lg-offset-1">
+                        <div class="header-form">
+                            <div class="submit-status"></div> <!-- submit status -->
+                            <ul class="header-form-heading">
+                                <li>
+                                    <i class="fa fa-send-o"></i>
+                                    <p>Aenean dictum pharetra nibh sodales luctus felis vulputate aliquet quam.</p>
+                                </li>
+                                <li>
+                                    <i class="fa fa-heart-o"></i>
+                                    <p>Aenean dictum pharetra nibh sodales luctus felis vulputate aliquet quam.</p>
+                                </li>
+                            </ul>
+                            <form method="post" action="#" class="affa-form-signup">
+                                <input type="text" name="name" id="header_form_name" placeholder="Correo">
+                                <input type="text" name="email" id="header_form_email" placeholder="Repetir Correo">
+                                <input type="submit" name="submit" value="UNASÉ AHORA!" class="btn-medium">
+                            </form>
+                            <p class="txt-desc">We don’t share your personal info with anyone. Check out our <a href="#">Privacy Policy</a> for more information.</p>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- .row end -->
+
+            </div>
+            <!-- .container end -->
+
+        </div>
+        <!-- .header-content end -->
+
+    </div>
+    <!-- .header-overlay end -->
+
+</header>
+<!-- #header end -->
+
+@yield('content')
+
+<!-- #footer -->
+<footer id="footer">
+
+    <!-- .container -->
+    <div class="container">
+
+        <p class="copyright-txt">&copy; 2016 Copyrights by <a href="http://affapress.com/" target="_blank">Affapress</a> - All rights reserved.</p>
+
+        <div class="socials">
+            <a href="#" title="Facebook" class="link-facebook"><i class="fa fa-facebook"></i></a>
+            <a href="#" title="Twitter" class="link-twitter"><i class="fa fa-twitter"></i></a>
+            <a href="#" title="Google Plus" class="link-google-plus"><i class="fa fa-google-plus"></i></a>
+        </div>
+
+    </div>
+    <!-- .container end -->
+
+</footer>
+<!-- #footer end -->
+
+<div id="customize">
+    <div class="wrap-customize">
+        <div class="customize-panel">
+            <h5>Header</h5>
+            <div class="customize-list-link">
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/stretched/header_1/" title="Layout Stretched 1"><i class="fa fa-caret-right"></i> Stretched 1</a>
+                <a href="index-2.html" title="Layout Stretched 2"><i class="fa fa-caret-right"></i> Stretched 2</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/stretched/header_3/" title="Layout Stretched 3"><i class="fa fa-caret-right"></i> Stretched 3</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/stretched/header_4/" title="Layout Stretched 4"><i class="fa fa-caret-right"></i> Stretched 4</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/stretched/header_5/" title="Layout Stretched 5"><i class="fa fa-caret-right"></i> Stretched 5</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/stretched/header_6/" title="Layout Stretched 6"><i class="fa fa-caret-right"></i> Stretched 6</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/boxed/header_1/" title="Layout Boxed 1"><i class="fa fa-caret-right"></i> Boxed 1</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/boxed/header_2/" title="Layout Boxed 2"><i class="fa fa-caret-right"></i> Boxed 2</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/boxed/header_3/" title="Layout Boxed 3"><i class="fa fa-caret-right"></i> Boxed 3</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/boxed/header_4/" title="Layout Boxed 4"><i class="fa fa-caret-right"></i> Boxed 4</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/boxed/header_5/" title="Layout Boxed 5"><i class="fa fa-caret-right"></i> Boxed 5</a>
+                <a href="http://affapress.com/templates/conversi_professional_conversion_landing_page/html/boxed/header_6/" title="Layout Boxed 6"><i class="fa fa-caret-right"></i> Boxed 6</a>
+            </div>
+        </div>
+        <div class="customize-panel">
+            <h5>Colors</h5>
+            <div class="customize-list-color">
+                <a href="#" class="blue" title="Blue" style="background-color:#3ebcfa;"></a>
+                <a href="#" class="green" title="Green" style="background-color:#44d86e;"></a>
+                <a href="#" class="pink" title="Pink" style="background-color:#fb53c2;"></a>
+                <a href="#" class="purple" title="Purple" style="background-color:#c95ef5;"></a>
+                <a href="#" class="red" title="Red" style="background-color:#85C1E9;"></a>
+                <a href="#" class="yellow" title="Yellow" style="background-color:#f7b943;"></a>
+            </div>
+        </div>
+    </div>
+    <div class="popup-open"><i class="fa fa-gear"></i></div>
+</div>
+
+<!--[if lt IE 8]>
+<div class="browser-notice">
+    <div class="container">
+        <div class="text">
+            <h1>Internet Explorer Out To Date</h1>
+            <p>Please update your Internet Explorer browser with a newer version (Internet Explorer 8 above) now!</p>
+            <span>You can download it <a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie" target="_blank">here....</a></span>
+        </div>
+    </div>
+</div>
+<![endif]-->
+
+<!-- JavaScripts -->
+<script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.min.js"></script>
+<script type="text/javascript" src="js/smoothscroll.js"></script>
+<script type="text/javascript" src="js/response.min.js"></script>
+<script type="text/javascript" src="js/jquery.placeholder.min.js"></script>
+<script type="text/javascript" src="js/jquery.fitvids.js"></script>
+<script type="text/javascript" src="js/jquery.imgpreload.min.js"></script>
+<script type="text/javascript" src="js/waypoints.min.js"></script>
+<script type="text/javascript" src="js/slick.min.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
+<script type="text/javascript" src="js/jquery.fancybox-media.js"></script>
+<script type="text/javascript" src="js/jquery.counterup.min.js"></script>
+<script type="text/javascript" src="js/parallax.min.js"></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDE7s3AMgMt8XkGB3xZemuH4MU_HKNSpFw"></script>
+<script type="text/javascript" src="js/gmaps.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+
+<script type="text/javascript">
+    var map;
+
+    map = new window.GMaps({
+        div: '#companyMap',
+        lat: -12.0411925,
+        lng: -77.0282043,
+        scrollwheel: false,
+        zoomControl: false,
+        disableDoubleClickZoom: false,
+        disableDefaultUI: true
+    });
+
+    map.addMarker({
+        lat: -12.042,
+        lng: -77.028333,
+        title: 'Company Name',
+        infoWindow: {
+            content: 'Your Company Name'
+        }
+    });
+</script>
+
+</body>
+</html>
